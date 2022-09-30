ff64be8e48ff315829dcb0a3fe0affeb24c6a997
Kira <1176087034@qq.com>
2022-09-30 13:43:33 +0800

    9.30 PM1:40

--- TodoApi/GlobalSuppressions.cs
+++ TodoApi/GlobalSuppressions.cs
@@ -2,7 +2,6 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-// a some change
 
 using System.Diagnostics.CodeAnalysis;
 
