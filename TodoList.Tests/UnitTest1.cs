add7e03fbf1a108d5dd3b7fcd2065f1f4c427ea1
Kira0606 <1176087034@qq.com>
2022-10-08 11:00:29 +0800

    Update UnitTest1.cs
    10.8 AM11:00

--- TodoList.Tests/UnitTest1.cs
+++ TodoList.Tests/UnitTest1.cs
@@ -1,26 +1,26 @@
-using Microsoft.EntityFrameworkCore;
-using TodoApi.Context;
-using TodoApi.Models;
-using TodoApi.Services;
-
 namespace TodoList.Tests
 {
+    using Microsoft.EntityFrameworkCore;
+    using TodoApi.Context;
+    using TodoApi.Models;
+    using TodoApi.Services;
+
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
-
         }
 
-        [Test]
-        public async Task Shuould_Return_All_Itemss_ById()
+        [Test] // Get方法
+        public void Shuould_Return_All_Itemss_ById()
         {
-            //ʹ���ڴ����ݿ�
+            // 使用内存数据库
             var optins = new DbContextOptionsBuilder<TodoContext>()
                 .UseInMemoryDatabase(databaseName: "TodolistItems")
                 .Options;
-            //mock TodoContext
+
+            // mock TodoContext
             using var context = new TodoContext(optins);
 
             context.TodoListItems.Add(new TodoListItem
@@ -29,7 +29,7 @@ namespace TodoList.Tests
                 AddDate = DateTime.Now,
                 IsDone = true,
                 UserID = 1,
-                Title = "sss"
+                Title = "sss1",
             });
             context.TodoListItems.Add(new TodoListItem
             {
@@ -37,24 +37,86 @@ namespace TodoList.Tests
                 AddDate = DateTime.Now,
                 IsDone = false,
                 UserID = 2,
-                Title = "friday"
+                Title = "friday1",
             });
             context.SaveChanges();
 
-
-            //mock service
+            // mock service
             SqlHelper sqlHelper = new SqlHelper(context);
-            //  var result = await sqlHelper.GetItemsById(1);
-            var results = sqlHelper.GetUserId(1);
-            //   var actualResult = results.Value;
 
-            //Assert.IsTrue(1 == Convert.ToInt32((actualResult).UserID));
-            //Assert.IsTrue("sss" == actualResult.Title);
-            //Assert.IsTrue(actualResult.IsDone);
-            Assert.Equals(1, results.Count());
-            Assert.That(results.ToList()[0].Title, Is.EqualTo("sss"));
+            var results = sqlHelper.GetUserId(1);
+
+            Assert.That(results.Count(), Is.EqualTo(1));
+            Assert.That(results.ToList()[0].Title, Is.EqualTo("sss1"));
+          //  Assert.IsTrue(results.ToList()[0].Title == "sss1");
         }
 
+        [Test] // post方法
+        public void PostItems_ByUserId()
+        {
+            var optins = new DbContextOptionsBuilder<TodoContext>()
+               .UseInMemoryDatabase(databaseName: "TodolistItems")
+               .Options;
 
+            using var context = new TodoContext(optins);
+
+            SqlHelper sqlHelper = new SqlHelper(context);
+
+            var results = sqlHelper.PostItems(new TodoListItem
+            {
+                OrdersId = 2,
+                AddDate = DateTime.Now,
+                IsDone = false,
+                UserID = 2,
+                Title = "sad",
+            });
+
+            var actresults = sqlHelper.GetUserId(2);
+
+            Assert.That(actresults.ToList()[0].Title, Is.EqualTo("sad"));
+        }
+
+        [Test] // put 方法
+        public void Put_ByTitle()
+        {
+            var optins = new DbContextOptionsBuilder<TodoContext>()
+               .UseInMemoryDatabase(databaseName: "TodolistItems")
+               .Options;
+
+            using var context = new TodoContext(optins);
+
+            SqlHelper sqlHelper = new SqlHelper(context);
+
+            var results = sqlHelper.UpdateItemsById(2, new TodoListItem
+            {
+                OrdersId = 2,
+                AddDate = DateTime.Now,
+                IsDone = false,
+                UserID = 2,
+                Title = "sadly",
+            });
+
+            var actresults = sqlHelper.GetUserId(2);
+
+            Assert.That(actresults.ToList()[0].Title, Is.EqualTo("sadly"));
+        }
+
+        [Test]
+        public void Delete_ByTitle()
+        {
+            var optins = new DbContextOptionsBuilder<TodoContext>()
+              .UseInMemoryDatabase(databaseName: "TodolistItems")
+              .Options;
+
+            using var context = new TodoContext(optins);
+
+            SqlHelper sqlHelper = new SqlHelper(context);
+
+            var results = sqlHelper.DeleteItemsByTitle(2, "sad");
+
+            var actresults = sqlHelper.GetUserId(2);
+
+            Assert.GreaterOrEqual(actresults.Count(), 0);
+        }
     }
 }
